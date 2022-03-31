8719dfa3ff0023282df7169021cc10fa0e03d499
Carmen Montiel <carmencita-2601@hotmail.com>
2022-03-31 11:02:00 -0500

    Realicé código en js de la calculadora con medidas fijas

---
+++ figuras.js
@@ -0,0 +1,64 @@
+// código del cuadrado
+console.group("Cuadrados");
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado + "cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es: " + perimetroCuadrado + "cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El área del cuadrado es: " + areaCuadrado + "cm^2");
+console.groupEnd();
+
+// Código del triángulo
+console.group("Triángulos");
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+
+console.log(
+    "Los lados del triángulo miden: "
+    + ladoTriangulo1
+    + "cm, "
+    + ladoTriangulo2
+    + "cm, "
+    + baseTriangulo
+    + "cm^2"
+);
+
+const alturaTriangulo = 5.5;
+console.log("La altura del triángulo es: " + alturaTriangulo);
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
+console.log("El perímetro del triángulo es: " + perimetroTriangulo + "cm");
+
+const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+console.log("El área del triángulo es: " + areaTriangulo + "cm^2");
+
+console.groupEnd();
+
+// Código del círculo
+console.group("Círculos");
+
+// Radio
+const radioCirculo = 4;
+console.log("El radio del círculo es: " + radioCirculo + "cm");
+
+// Diámetro
+const diametroCirculo = radioCirculo * 2;
+console.log("El diametro del círculo es: " + diametroCirculo + "cm");
+
+// PI
+const PI = Math.PI;
+console.log("PI es: " + PI);
+
+// Circunferencia
+const perimetroCirlo = diametroCirculo * PI;
+console.log("El perímetro del círculo es: " + perimetroCirlo + "cm");
+
+// Área
+const areaCirculo = (radioCirculo * radioCirculo) * PI;
+console.log("El área del círculo es: " + areaCirculo + "cm^2");
+
+
+console.groupEnd();
